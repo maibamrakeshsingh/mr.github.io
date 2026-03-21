4a926c48bd6ceea0fff5689838bec42040150576
maibamrakeshsingh <maibamrakeshsingh31@gmail.com>
2026-03-21 15:33:32 +0000

    Deploying to gh-pages from @ maibamrakeshsingh/mr.github.io@cddffd44dc43e5854505c525caa67dabab665518 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,7 +38,7 @@ ninja.data = [{
             window.location.href = "/mr.github.io/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "Course materials, schedules, and resources for classes taught.",
           section: "Navigation",
           handler: () => {
